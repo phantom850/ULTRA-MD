d9f544d2f2cb0ddecf9932c0af883aec93532589
phantom850 <jegeded679@gmail.com>
2025-01-04 03:13:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.pairingNumber = 2348030520780 process.env.BOT_NUMBER || 'your number';  // put your number here
+global.pairingNumber = process.env.BOT_NUMBER || 'your number';  /2348030520780/ put your number here
 global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
 
 global.mods = []
